11864e119f8bb5970f47fed9f3e146a255c218d3
EduRamos95 <eduramos08.30@gmail.com>
2024-05-04 21:24:11 -0500

    Add: tailwind configuration

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  purge: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'], // Agrega las rutas de los archivos que usan Tailwind CSS
+  darkMode: false, // Opcional, dependiendo de tus necesidades
   theme: {
-    extend: {},
+    extend: {}, // Puedes extender o personalizar temas aquí
   },
-  plugins: [],
-}
+  variants: {
+    extend: {}, // Puedes extender o personalizar variantes aquí
+  },
+  plugins: [], // Puedes agregar plugins adicionales aquí
+};
+
 
