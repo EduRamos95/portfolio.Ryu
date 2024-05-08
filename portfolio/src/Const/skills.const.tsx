fe8e46c5256b8c2cbb1f9b1969f78ec0ecfaa476
EduRamos95 <eduramos08.30@gmail.com>
2024-05-08 16:25:25 -0500

    Add: skils.const export elements icons

---
+++ portfolio/src/Const/skills.const.tsx
@@ -0,0 +1,31 @@
+import { VscTerminalLinux } from "react-icons/vsc";
+import { SiAdobeillustrator, SiExpress, SiFlask, SiGnubash, SiIntellijidea, SiJavascript, SiMongodb, SiMysql, SiNextdotjs, SiPostman, SiPrisma, SiReact, SiVim, SiVisualstudiocode } from "react-icons/si";
+import { FaAngular, FaCss3Alt, FaFigma, FaGitAlt, FaHtml5, FaJava, FaNodeJs, FaPython } from "react-icons/fa";
+import { BiLogoPostgresql } from "react-icons/bi";
+
+export const allSkills:JSX.Element[] = [
+    <SiGnubash />,
+    <VscTerminalLinux />,
+    <FaGitAlt />,
+    <FaHtml5 />,
+    <FaCss3Alt />,
+    <SiJavascript />,
+    <SiReact />,
+    <FaAngular />,
+    <SiNextdotjs />,
+    <FaPython />,
+    <FaJava />,
+    <SiPrisma />,
+    <FaNodeJs />,
+    <SiExpress />,
+    <SiFlask />,
+    <SiMysql />,
+    <BiLogoPostgresql />,
+    <SiMongodb />,
+    <FaFigma />,
+    <SiAdobeillustrator />,
+    <SiPostman />,
+    <SiVim />,
+    <SiVisualstudiocode />,
+    <SiIntellijidea />,
+];
