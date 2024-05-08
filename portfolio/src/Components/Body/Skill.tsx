bdb923c312bc7648428c0f4744ba1c3b543ac70c
EduRamos95 <eduramos08.30@gmail.com>
2024-05-08 16:25:02 -0500

    Add: skill section

--- portfolio/src/Components/Body/Skill.tsx
+++ portfolio/src/Components/Body/Skill.tsx
@@ -1,7 +1,12 @@
+import SkillList from "../Commons/Card/hardskillList";
+import '../Commons/styles/Skill.scss';
+
 function Skills() {
+
     return (
         <div id="skills" className="cardBlock">
             <p className="text-white letter">Skills</p>
+            <SkillList/>
         </div>
     )
 }
