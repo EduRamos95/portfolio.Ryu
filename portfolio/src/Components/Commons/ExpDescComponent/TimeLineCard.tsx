6d8aa188785cd96572c80c82261f790bac67bd2e
EduRamos95 <eduramos08.30@gmail.com>
2024-05-09 02:26:57 -0500

    update: timeLineCard

--- portfolio/src/Components/Commons/ExpDescComponent/TimeLineCard.tsx
+++ portfolio/src/Components/Commons/ExpDescComponent/TimeLineCard.tsx
@@ -1,24 +1,30 @@
 import { WorkData } from "./Exp.model";
+import './TimeLineCard.scss';
 
 function TimelineCard({ company, job, dateStart, dateEnd, descriptions, stack }:WorkData) {
     return (
       <div className="timeline-card">
-        <h3 className="timeline-card__title">{company}</h3>
+        <div className="timeline-card__sup">
+            <h3 className="timeline-card__sup__title">{company}</h3>
+            <p className="timeline-card__sup__date">{dateStart} - {dateEnd}</p>
+        </div>
         <hr />
-        <p className="timeline-card__cargo">{job}</p>
-        <p className="timeline-card__fecha">{dateStart} - {dateEnd}</p>
+        <p className="timeline-card__job">{job}</p>
         <ul>
           {descriptions.map((activity, index) => (
             <li key={`${"description"}${index}`}>{activity}</li>
           ))}
         </ul>
-        <p>Stack:</p>
         <div className="timeline-card__stack">
-          {stack.map((tecnology, index) => (
-            <div key={`${"stack"}${index}`} className="timeline-card__stack__element">
-                {tecnology}
+            <p>Stack:</p>
+            <div className="timeline-card__stack__group">
+
+            {stack.map((tecnology, index) => (
+                <div key={`${"stack"}${index}`} className="timeline-card__stack__group__element">
+                    {tecnology}
+                </div>
+            ))}
             </div>
-          ))}
         </div>
       </div>
     );
