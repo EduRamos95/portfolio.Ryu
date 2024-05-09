e2776ae55e864fd9760b0dd2a5f23fe7544c7360
EduRamos95 <eduramos08.30@gmail.com>
2024-05-08 22:31:22 -0500

    update: charge data in section Experience

--- portfolio/src/Components/Body/Experience.tsx
+++ portfolio/src/Components/Body/Experience.tsx
@@ -1,7 +1,15 @@
+import { work1, work2, work3 } from "../Commons/ExpDescComponent/Exp.const";
+import { WorkData } from "../Commons/ExpDescComponent/Exp.model";
+import ExpWork from "../Commons/ExpDescComponent/ExpWork";
+
+
 function Experience() {
+    const works:WorkData[] = [work3,work2,work1];
+
     return (
         <div id="experience" className="cardBlock">
             <p className="text-white letter letter-tittle">Experience</p>
+            <ExpWork worksExp={works}/>
         </div>
     )
 }
